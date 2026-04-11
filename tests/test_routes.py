e1211ddb3e8c63568de7376dbb4b75cbbffa73d8
Teodor <teodorbriceanu@gmail.com>
2026-04-11 11:05:29 +0300

    adwawda

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -16,7 +16,7 @@ def client():
 def test_get_gigi(client):
     response = client.get("/gigi")
     assert response.status_code == 200
-    assert response.data.decode() == "this is gigi 2"
+    assert response.data.decode() == "this is gigi 2awdawd"
 
 
 user_data_signup = {
